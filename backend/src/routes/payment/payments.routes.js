8bf921ee75135752963e01a4a5d464eb28f15546
clintonictendency@gmail.com <clintonictendency@gmail.com>
2026-08-21 12:32:00 +0100

    Added vercel.nson file

--- backend/src/routes/payment/payments.routes.js
+++ backend/src/routes/payment/payments.routes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const { getPayment, verifyPayment } = require('../../controllers/payments/payment.controller');
 const router = express.Router();
+const path = require('path')
+
+const fileName = path.matchesGlob
+console.log(fileName);
+
 
 
 router.post('/initiate-payment', getPayment);
